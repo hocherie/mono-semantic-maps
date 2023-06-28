3b489d30beb65e783fa1a80fadfd799c9d929141
Cherie Ho <cherieho@br013.ib.bridges2.psc.edu>
2023-06-28 14:30:33 -0400

    extracting nuscenes labels, named by sample data. get cam_front and map

--- scripts/make_nuscenes_labels.py
+++ scripts/make_nuscenes_labels.py
@@ -10,6 +10,8 @@ from nuscenes import NuScenes
 from nuscenes.map_expansion.map_api import NuScenesMap
 from matplotlib import pyplot as plt 
 import cv2
+from tqdm.contrib.concurrent import process_map  # or thread_map
+from p_tqdm import p_map
 
 sys.path.append(os.path.abspath(os.path.join(__file__, '../..')))
 
@@ -19,7 +21,7 @@ from src.data.utils import get_visible_mask, get_occlusion_mask, transform, \
 import src.data.nuscenes.utils as nusc_utils
 
 
-def process_scene(nuscenes, map_data, scene, config):
+def process_scene(scene):
 
     # Get the map corresponding to the current sample data
     log = nuscenes.get('log', scene['log_token'])
@@ -27,22 +29,26 @@ def process_scene(nuscenes, map_data, scene, config):
 
     # Iterate over samples
     first_sample_token = scene['first_sample_token']
+    print("Processing scene:", scene['log_token'])
     for sample in nusc_utils.iterate_samples(nuscenes, first_sample_token):
         process_sample(nuscenes, scene_map_data, sample, config)
+    
 
 
 def process_sample(nuscenes, map_data, sample, config):
 
-    # Load the lidar point cloud associated with this sample
-    lidar_data = nuscenes.get('sample_data', sample['data']['LIDAR_TOP'])
-    lidar_pcl = nusc_utils.load_point_cloud(nuscenes, lidar_data)
+    # # Load the lidar point cloud associated with this sample
+    # lidar_data = nuscenes.get('sample_data', sample['data']['LIDAR_TOP'])
+    # lidar_pcl = nusc_utils.load_point_cloud(nuscenes, lidar_data)
 
-    # Transform points into world coordinate system
-    lidar_transform = nusc_utils.get_sensor_transform(nuscenes, lidar_data)
-    lidar_pcl = transform(lidar_transform, lidar_pcl)
+    # # Transform points into world coordinate system
+    # lidar_transform = nusc_utils.get_sensor_transform(nuscenes, lidar_data)
+    # lidar_pcl = transform(lidar_transform, lidar_pcl)
+    lidar_pcl = None 
 
     # Iterate over sample data
-    for camera in nusc_utils.CAMERA_NAMES:
+    # for camera in nusc_utils.CAMERA_NAMES:
+    for camera in ['CAM_FRONT']: # cherie: only interested in cam_front for visualization purposes
         sample_data = nuscenes.get('sample_data', sample['data'][camera])
         process_sample_data(nuscenes, map_data, sample_data, lidar_pcl, config)
 
@@ -72,30 +78,14 @@ def process_sample_data(nuscenes, map_data, sample_data, lidar, config):
         masks = np.concatenate([map_masks, obj_masks], axis=0) # (15, 196, 200)
 
 
-        plt_row = 3
-        plt_col = 6
-
-        masks_title = nusc_utils.STATIC_CLASSES + nusc_utils.NUSCENES_CLASS_NAMES + ['others']
-
-        # import pdb; pdb.set_trace()
-        # print(sample_data['filename'])
-        # import pdb; pdb.set_trace()
         cam_img_filepath = config.dataroot + '/' + sample_data['filename']
         cam_img = cv2.imread(cam_img_filepath)
-        cv2.imwrite(os.path.join(os.path.expandvars(config.label_root), 'road_' + str(sample_data['timestamp']) + '_img.png'),
+        cv2.imwrite(os.path.join(os.path.expandvars(config.label_root), 'cam', str(sample_data['sample_token']) + '.png'),
                     cam_img)
-        # import time 
-        # start_time = time.time()
-        # plt.clf()
-        # for map_i in range(masks.shape[0]):
-        #     plt.subplot(plt_row, plt_col, map_i+1)
-
-        #     plt.imshow(masks[map_i].astype(np.int32)*255)
-        #     plt.title(masks_title[map_i])
-        # plt.savefig(os.path.join(os.path.expandvars(config.label_root), 'all_' + 
-        #                         sample_data['token'] + '.png'))
-        cv2.imwrite(os.path.join(os.path.expandvars(config.label_root), 'road_' + str(sample_data['timestamp']) + '.png'),
-                        (masks[4] | masks[5]).astype(np.int32)*255)
+        
+        # TODO: confirm map masks are correct (currently combingin masks[4] and masks[5])
+        cv2.imwrite(os.path.join(os.path.expandvars(config.label_root), 'map', str(sample_data['sample_token']) + '.png'),
+                        ((masks[4] | masks[5]).astype(np.int32)*255).transpose())
         # print(sample_data['timestamp'])
         # print("Time taken (s):", np.round(time.time() - start_time, 2))
 
@@ -162,6 +152,7 @@ def load_map_data(dataroot, location):
 
 
 if __name__ == '__main__':
+    global nuscenes, map_data, config
 
     # Load the default configuration
     config = get_default_configuration()
@@ -178,12 +169,17 @@ if __name__ == '__main__':
     # Create a directory for the generated labels
     output_root = os.path.expandvars(config.label_root)
     os.makedirs(output_root, exist_ok=True)
+    os.makedirs(output_root + '/cam/', exist_ok=True)
+    os.makedirs(output_root + '/map/', exist_ok=True)
     
    # print(nuscenes.scene)
     # Iterate over NuScene scenes
     print("\nGenerating labels...")
-    for scene in tqdm(nuscenes.scene):
-        process_scene(nuscenes, map_data, scene, config)
+
+    r = process_map(process_scene, nuscenes.scene, max_workers=4)
+    # r = p_map(process_scene, nuscenes, map_data, nuscenes.scene, config)
+    # for scene in tqdm(nuscenes.scene):
+    #     process_scene(nuscenes, map_data, scene, config)
 
 
 
